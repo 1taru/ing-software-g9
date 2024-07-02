366f60cfa2d33d15b020c1091258eab23fd4ed79
Rodrigo Yañez <rodrigoye90@gmail.com>
2024-07-01 21:38:54 -0400

    temp updated

--- client/src/pages/inventory/CreateMaterial.jsx
+++ client/src/pages/inventory/CreateMaterial.jsx
@@ -5,17 +5,39 @@ import { useNavigate } from 'react-router-dom';
 
 export const CreateMaterial = () => {
   const navigate = useNavigate();
-  const [data, setData] = useState({ name: '', price: '', quantity: '', location: ''});
-  
+  const [data, setData] = useState({
+    name: '',
+    doorType: '',
+    category: '',
+    materialType: '',
+    height: '',
+    width: '',
+    length: '',
+    quantity: '',
+    price: '',
+    location: ''
+  });
+
   const createMaterial = async (e) => {
     e.preventDefault();
-    const { name, price, quantity, location } = data;
+    const { name, doorType, category, materialType, height, width, length, quantity, price, location } = data;
     try {
-      const { data: response } = await axios.post('http://localhost:8000/inventory/createMaterial', { name, price, quantity, location });
+      const { data: response } = await axios.post('http://localhost:8000/inventory/createMaterial', { name, doorType, category, materialType, height, width, length, quantity, price, location });
       if (response.error) {
         toast.error(response.error);
       } else {
-        setData({ name: '', price: '', quantity: '', location: ''});
+        setData({
+          name: '',
+          doorType: '',
+          category: '',
+          materialType: '',
+          height: '',
+          width: '',
+          length: '',
+          quantity: '',
+          price: '',
+          location: ''
+        });
         toast.success('Material agregado exitosamente.');
         navigate('/createMaterial');
       }
@@ -31,19 +53,78 @@ export const CreateMaterial = () => {
         <label className="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Nombre del Material</label>
         <input
           type='text'
-          placeholder='Clavo'
+          placeholder='Material'
           value={data.name}
           onChange={(e) => setData({ ...data, name: e.target.value })}
           className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
         />
       </div>
       <div className="mb-5">
-        <label className="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Precio</label>
+        <label className="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Tipo de Puerta</label>
+        <select
+          value={data.doorType}
+          onChange={(e) => setData({ ...data, doorType: e.target.value })}
+          className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+        >
+          <option value=''>Seleccione</option>
+          <option value='F60'>F60</option>
+          <option value='F30'>F30</option>
+          <option value='MDF'>MDF</option>
+          <option value='TERCIADO'>TERCIADO</option>
+        </select>
+      </div>
+      <div className="mb-5">
+        <label className="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Categoría</label>
+        <select
+          value={data.category}
+          onChange={(e) => setData({ ...data, category: e.target.value })}
+          className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500">
+          <option value=''>Seleccione</option>
+          <option value='Plancha'>Plancha</option>
+          <option value='Bastidor'>Bastidor</option>
+          <option value='Relleno'>Relleno</option>
+        </select>
+      </div>
+      <div className="mb-5">
+        <label className="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Tipo de Material</label>
+        <input
+          type='text'
+          placeholder='Tipo Material'
+          value={data.materialType}
+          onChange={(e) => setData({ ...data, materialType: e.target.value })}
+          className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+        />
+      </div>
+      <div className="mb-5">
+        <label className="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Espesor</label>
         <input
           type='number'
-          placeholder='100'
-          value={data.price}
-          onChange={(e) => setData({ ...data, price: e.target.value })}
+          min='1'
+          placeholder='Espesor de material (Milimetros)'
+          value={data.height}
+          onChange={(e) => setData({ ...data, height: e.target.value })}
+          className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+        />
+      </div>
+      <div className="mb-5">
+        <label className="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Ancho</label>
+        <input
+          type='number'
+          min='1'
+          placeholder='Anchura de material (Milimetros)'
+          value={data.width}
+          onChange={(e) => setData({ ...data, width: e.target.value })}
+          className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+        />
+      </div>
+      <div className="mb-5">
+        <label className="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Largo</label>
+        <input
+          type='number'
+          min='1'
+          placeholder='Largo de material (Milimetros)'
+          value={data.length}
+          onChange={(e) => setData({ ...data, length: e.target.value })}
           className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
         />
       </div>
@@ -51,20 +132,36 @@ export const CreateMaterial = () => {
         <label className="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Cantidad</label>
         <input
           type='number'
-          placeholder='5000'
+          min='1'
+          placeholder='Cantidad'
           value={data.quantity}
           onChange={(e) => setData({ ...data, quantity: e.target.value })}
           className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
         />
       </div>
       <div className="mb-5">
-        <label className="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Ubicación</label>
-        <textarea
-          placeholder='Bodega 1'
-          value={data.location}
-          onChange={(e) => setData({ ...data, location: e.target.value })}
+        <label className="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Precio</label>
+        <input
+          type='number'
+          min='1'
+          placeholder='Precio de producto'
+          value={data.price}
+          onChange={(e) => setData({ ...data, price: e.target.value })}
           className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
         />
+      </div>
+      
+      <div className="mb-5">
+        <label className="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Ubicación</label>
+        <select
+          value={data.location}
+          onChange={(e) => setData({ ...data, location: e.target.value })}
+          className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500">
+          <option value=''>Sin ubicacion</option>
+          <option value='Bodega 1'>Bodega 1</option>
+          <option value='Bodega 2'>Bodega 2</option>
+          <option value='Bodega 3'>Bodega 3</option>
+        </select>
       </div>
       <button type='submit' className="text-white bg-green-700 hover:bg-green-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm w-full sm:w-auto px-5 py-2.5 text-center dark:bg-lime-800 dark:hover:bg-lime-900 dark:focus:ring-blue-800">Agregar Material</button>
     </form>
